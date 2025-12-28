1c2dbd1be19ececc36174c9fc0bb5e93a7493790
Mohammed Duaibes <m.duaibes@acico.com.kw>
2025-12-29 01:49:13 +0300

    Update Prestige build.gradle.kts to use cloudstream DSL

--- Prestige/build.gradle.kts
+++ Prestige/build.gradle.kts
@@ -1,35 +1,13 @@
-// Prestige Plugin build configuration
+version = 1
 
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
+cloudstream {
+    authors = listOf("mrmakrider")
 
-repositories {
-    mavenCentral()
-    maven { url = uri("https://jitpack.io") }
-}
+    language = "ar"
 
-android {
-    namespace = "com.prestige"
-    compileSdk = 34
-    defaultConfig {
-        minSdk = 21
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs = listOf("-XXLanguage:+BreakContinueInInlineLambdas")
-    }
-}
+    status = 1
 
-dependencies {
-    val cloudstream by configurations
-    implementation("com.google.android.material:material:1.13.0")
-    implementation("androidx.browser:browser:1.9.0")
-    implementation("androidx.room:room-ktx:2.8.0")
-    cloudstream("com.lagradost:cloudstream3:pre-release")
+    tvTypes = listOf("TvSeries", "Movie")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=hp.brstej.com&sz=%size%"
 }
